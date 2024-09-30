cf49e09f2d1a9cb1405bbe58a0679f0a748ddb78
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2024-09-30 14:04:12 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,8 +18,8 @@ extensions.configure<StonecutterSettings> {
     kotlinController = true
     centralScript = "build.gradle.kts"
     shared {
-        versions("1.20.1", "1.20.4", "1.20.6", "1.21")
-        vcsVersion = "1.21"
+        versions("1.20.1", "1.20.4", "1.20.6", "1.21.1")
+        vcsVersion = "1.21.1"
     }
     create(rootProject)
 }
